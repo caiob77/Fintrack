2019d8023287351f8b3985de708493190842fb54
Caiob77 <caio.beniel55@gmail.com>
2026-02-11 01:13:09 -0400

    feat: update @tanstack/react-query and add ReactQueryDevtools for enhanced query management

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Toaster } from 'sonner'
 import { AuthContextProvider } from './contexts/auth.jsx'
 import { AvatarContextProvider } from './contexts/avatar.jsx'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient()
 
@@ -29,6 +30,7 @@ createRoot(document.getElementById('root')).render(
                <Toaster />
             </AvatarContextProvider>
          </AuthContextProvider>
+         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
   </StrictMode>,
 )
